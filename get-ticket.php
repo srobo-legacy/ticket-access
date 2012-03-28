9464d6d242361f178d533ebef44b094e303391bf
Robert Spanton <rspanton@zepler.net>
2012-03-28 23:05:57 +0100

    Add time, date, and URL of the competition to the ticket
    So the competitors know when it is.

--- get-ticket.php
+++ get-ticket.php
@@ -9,7 +9,7 @@ $ticket_name = "$base_dir/$user.pdf";
 if (!file_exists($ticket_name)) {
     # run the generation script
 
-    exec("cd $base_dir ; python generate.py $user -o $ticket_name 2>&1", $output, $rv);
+    exec("cd tickets ; python generate.py $user -y 2012 -d 'April 14th-15th (Doors open 9:30)' -l 'https://www.studentrobotics.org/comp' -o $ticket_name 2>&1", $output, $rv);
     if (!file_exists($ticket_name)) {
         header('HTTP/1.1 403 Forbidden');
         header('Content-type: image/jpeg');
